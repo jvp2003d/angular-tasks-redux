a14ccad644e1235d87a2f4958113717c0af7d6b5
Jorge Vera Paz <jvp2003d@gmail.com>
2022-06-15 12:55:40 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TasksComponent,
+    NewTaskComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
